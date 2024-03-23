056137e0b4fe6990fba96742892969ef1aa63ea6
AserZakaria <asserzakaria@gmail.com>
2024-03-22 23:38:56 -0700

    A

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,8 +0,0 @@
-exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow',
-  path        => ['/bin', '/usr/bin'], # Add additional paths if necessary
-  refreshonly => true,                  # Only run when notified
-}
-notify { 'Terminate killmenow process':
-  require => Exec['killmenow'],
-}
